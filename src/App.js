f99b68fb87d85bd2451c300f81f1eecc287d5ab9
Marcin <marcin.markiewicz@itechart-group.com>
2021-07-28 13:41:28 +0200

    HuskyTest

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit
-                    <code>src/App.js</code>
-                    and save to reload.
-                <p>
-                <p>
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <p>
+                                Edit
+                                <code>src/App.js</code>
+                                and save to reload.
+                            <p>
+                            <p>
                     Edit
                     <code>src/App.js</code>
                     and save to reload.
