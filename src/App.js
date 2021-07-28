ab64073930c5e45268cd1384c521012ace875ee3
Marcin <marcin.markiewicz@itechart-group.com>
2021-07-28 13:41:05 +0200

    HuskyTest

--- src/App.js
+++ src/App.js
@@ -11,7 +11,43 @@ function App() {
                     Edit
                     <code>src/App.js</code>
                     and save to reload.
+                <p>
+                <p>
+                    Edit
+                    <code>src/App.js</code>
+                    and save to reload.
                 </p>
+                <p>
+                    Edit
+                    <code>src/App.js</code>
+                    and save to reload.
+                </p>
+                <p>
+                    Edit
+                    <code>src/App.js</code>
+                    and save to reload.
+                </p>
+                <p>
+                    Edit
+                    <code>src/App.js</code>
+                    and save to reload.
+                </p>
+                <p>
+                    Edit
+                    <code>src/App.js</code>
+                    and save to reload.
+                </p>
+                <p>
+                    Edit
+                    <code>src/App.js</code>
+                    and save to reload.
+                </p>
+                <p>
+                    Edit
+                    <code>src/App.js</code>
+                    and save to reload.
+                </p>
+
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
